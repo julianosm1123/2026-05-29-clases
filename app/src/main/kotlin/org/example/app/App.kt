521044521384126805b2489af890a05e24eb706e
julianosm1123 <sanmartin.a.julian@gmail.com>
2026-06-01 09:04:38 -0300

    fecha definida en app.kt

---
+++ app/src/main/kotlin/org/example/app/App.kt
@@ -0,0 +1,11 @@
+package org.example.app
+import  org.example.app.Oggi
+class App{
+    fun darfecha(){
+        var ddd:Int=1
+        var mmm:Int=6
+        var aaa:Int=2026
+        var fecha:Oggi=Oggi(ddd,mmm,aaa)
+        println(fecha)
+    }
+}
